9dc879daa52a5ca7f3e5d05dfc518a78628f0e6f
chandhappy <35898839+chandhappy@users.noreply.github.com>
2019-01-04 14:16:15 +0530

    FinalAzureCopy
    FinalAzureCopy

--- AzureDataCopy/AzureDataCopy/Program.cs
+++ AzureDataCopy/AzureDataCopy/Program.cs
@@ -16,7 +16,7 @@ namespace AzureDataCopy
 
             //Create COM Objects. Create a COM object for everything that is referenced
             Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\INCBASHA\Desktop\AutoDR_Bugs.xlsx");
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\INCBASHA\Desktop\Singapore Data Migration\AzureDataURLS.xlsx");
             Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
             Excel.Range xlRange = xlWorksheet.UsedRange;
 
@@ -25,14 +25,13 @@ namespace AzureDataCopy
 
             //iterate over the rows and columns and print to the console as it appears in the file
             //excel is not zero based!!
-            for (int i = 1; i <= 2; i++)
+            for (int i = 1; i <= rowCount; i++)
             {
                 //write the value to the console
-                if (xlRange.Cells[i, 3] != null && xlRange.Cells[i, 3].Value2 != null)
-                {
+                if (xlRange.Cells[i, 1] != null && xlRange.Cells[i, 2] != null && xlRange.Cells[i, 1].Value2 != null && xlRange.Cells[i, 2].Value2 != null) { 
+  
                     strCmdText = null;
-                    strCmdText = "/k cd C:/Program Files (x86)/Microsoft SDKs/Azure/AzCopy" + "& AzCopy /Source:\"" + xlRange.Cells[i, 2].Value2.ToString() + "\" /Dest:\"" + xlRange.Cells[i, 3].Value2.ToString() + "\" /SourceKey:" + SourceKey + " /DestKey:" + DestKey + "  /Y /V:C:/Desktop/testServer/azcopy_" + System.DateTime.Now.ToShortDateString() + ".log";
-
+                    strCmdText = "/k cd C:/Program Files (x86)/Microsoft SDKs/Azure/AzCopy" + "& AzCopy /Source:\"" + xlRange.Cells[i, 1].Value2.ToString() + "\" /Dest:\"" + xlRange.Cells[i, 2].Value2.ToString() + "\" /SourceKey:" + SourceKey + " /DestKey:" + DestKey + "  /Y /V:C:/Desktop/testServer/azcopy_" + System.DateTime.Now.ToShortDateString() + ".log";
                     Console.WriteLine(strCmdText);
                     //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
                     System.Threading.Thread.Sleep(8000);
